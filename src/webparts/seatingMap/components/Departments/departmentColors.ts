f14cde7ffb41a5410fdbdb93538b9f5555ed661f
Shawn Pantzlaff <2001shawnp@gmail.com>
2024-08-08 14:10:47 +0500

    checkpoint99

---
+++ src/webparts/seatingMap/components/Departments/departmentColors.ts
@@ -0,0 +1,19 @@
+
+const departmentColors = {
+    '1': 'red',
+    '2': 'green',
+    '3': 'blue',
+    '4': 'orange',
+    '5': 'purple',
+    '6': 'pink',
+    '7': 'black',
+    '8': 'yellow',
+    '9': 'cyan',
+    '10': 'magenta',
+    '11': 'brown',
+    '12': 'lime',
+    '13': 'teal'
+};
+
+
+export default departmentColors
